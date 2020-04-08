99fc998590af323aa50824b4d08ac8e4c06d2bd9
Martín Campuzano <martin.campuzano.flores@gmail.com>
2020-04-08 11:28:30 -0400

    First Commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:novis_compiler/novis_compiler.dart' as novis_compiler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${novis_compiler.calculate()}!');
+}
---
+++ test/novis_compiler_test.dart
@@ -0,0 +1,8 @@
+import 'package:novis_compiler/novis_compiler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
